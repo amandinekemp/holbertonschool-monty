4c102aa405b4587a0dcf8b392d4dbd38b6fafef0
DDoudine <amandinekemp@gmail.com>
2024-01-04 10:29:46 +0100

    add function atoi and error message

--- push.c
+++ push.c
@@ -1,18 +1,46 @@
 #include "monty.h"
 
 /**
- * push - Pushes an integer onto the stack.
- * @stack: A pointer to a pointer to the stack structure.
- * @line_number: The integer to be pushed onto the stack.
- * Return: 0 if successful, 1 otherwise.
- */
+* push - Pushes an integer onto the stack.
+* @stack: A pointer to a pointer to the stack structure.
+* @line_number: The integer to be pushed onto the stack.
+* Return: 0 if successful, 1 otherwise.
+*/
 
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new = malloc(sizeof(stack_t));
-	if (new == NULL)
-		return;
-	new->n = line_number;
-	new->next = *stack;
-	*stack = new;
+	stack_t *new_node;
+	char *token;
+
+	int n;
+
+	token = strtok(NULL, " \n\t");
+	if (token == NULL)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	n = atoi(token);
+	if (n == 0 && strcmp(token, "0") != 0)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
 }
